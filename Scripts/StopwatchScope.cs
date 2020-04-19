64d792d6b9ccd9368d699063349924e168062306
baba_s <shotababa1206@gmail.com>
2020-04-19 20:24:55 +0900

    fix

--- Scripts/StopwatchScope.cs
+++ Scripts/StopwatchScope.cs
@@ -3,16 +3,34 @@ using System.Diagnostics;
 
 namespace UniStopwatchScope
 {
+	/// <summary>
+	/// 経過時間を計測するクラス
+	/// </summary>
 	public sealed class StopwatchScope : IDisposable
 	{
+		//==============================================================================
+		// デリゲート
+		//==============================================================================
 		public delegate void OnCompleteCallback( string name, TimeSpan elapsed );
 
+		//==============================================================================
+		// 変数(readonly)
+		//==============================================================================
 		private readonly string             m_name;
 		private readonly Stopwatch          m_stopwatch = new Stopwatch();
 		private readonly OnCompleteCallback m_onComplete;
 
+		//==============================================================================
+		// イベント(static)
+		//==============================================================================
 		public static event OnCompleteCallback OnComplete;
 
+		//==============================================================================
+		// 関数
+		//==============================================================================
+		/// <summary>
+		/// 計測を開始します
+		/// </summary>
 		public StopwatchScope( string name, OnCompleteCallback onComplete )
 		{
 			m_name       = name;
@@ -21,10 +39,16 @@ namespace UniStopwatchScope
 			m_stopwatch.Start();
 		}
 
+		/// <summary>
+		/// 計測を開始します
+		/// </summary>
 		public StopwatchScope( string name ) : this( name, null )
 		{
 		}
 
+		/// <summary>
+		/// 計測を終了します
+		/// </summary>
 		public void Dispose()
 		{
 			m_stopwatch.Stop();
